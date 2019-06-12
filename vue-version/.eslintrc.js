84b12f80acf741bd660b25cfc4320e5357bc9caa
Askie Lin <isme408@gmail.com>
2019-06-12 17:40:29 +0800

    remove prettier

--- vue-version/.eslintrc.js
+++ vue-version/.eslintrc.js
@@ -3,12 +3,10 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/airbnb", "prettier"],
-  plugins: ["prettier"],
+  extends: ["plugin:vue/essential", "@vue/airbnb"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "prettier/prettier": ["error"]
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   },
   parserOptions: {
     parser: "babel-eslint"
