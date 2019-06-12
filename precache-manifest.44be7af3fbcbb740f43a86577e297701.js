34a608a1da628156d12dc5e98ce6e9cf9756db13
Askie Lin <isme408@gmail.com>
2019-06-12 17:54:27 +0800

    deploy

---
+++ precache-manifest.44be7af3fbcbb740f43a86577e297701.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/todo/robots.txt"
+  },
+  {
+    "revision": "9baea0eb924ca5ce2b38",
+    "url": "/todo/js/chunk-vendors.3a52f26f.js"
+  },
+  {
+    "revision": "dab63e49ab3da8e70bf1",
+    "url": "/todo/js/app.adf849b9.js"
+  },
+  {
+    "revision": "bee291e38d49361d56c855cd122ba937",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "dab63e49ab3da8e70bf1",
+    "url": "/todo/css/app.5b0469d6.css"
+  }
+];
