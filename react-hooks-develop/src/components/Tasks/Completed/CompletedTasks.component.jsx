a16a6e56f2ebe514b0b414d30f0a37f1e1d29c8c
Askie Personal GitHub <isme408@gmail.com>
2021-06-24 15:31:06 +0800

    fix: className

--- react-hooks-develop/src/components/Tasks/Completed/CompletedTasks.component.jsx
+++ react-hooks-develop/src/components/Tasks/Completed/CompletedTasks.component.jsx
@@ -3,23 +3,23 @@ import './CompletedTasks.component.scss';
 
 const CompletedTasks = () => {
   return (
-    <section class='finished'>
+    <section className='finished'>
       <h2>已完成</h2>
-      <section class='tasks'>
+      <section className='tasks'>
         <span v-if='!hasTodoComplete'>尚未完成任何事項</span>
         {/* <div
-        class="task"
+        className="task"
         :data-index="todo.id"
         v-for="(todo, index) of todoCompleted"
         :key="`done-${index}`"
       >
-        <span class="task__checkbox" @click="updateTodoStatus(todo.id)">
+        <span className="task__checkbox" @click="updateTodoStatus(todo.id)">
           <font-awesome :icon="['fas', 'check']"/>
         </span>
-        <span type="text" class="task__content">{{ todo.todo }}</span>
+        <span type="text" className="task__content">{{ todo.todo }}</span>
         <input
           type="button"
-          class="task__button task__delete"
+          className="task__button task__delete"
           value="刪除"
           @click="deleteTodo(todo.id)"
         >
