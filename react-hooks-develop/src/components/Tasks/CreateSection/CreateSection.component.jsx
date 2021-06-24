c83b85d542da33ed9803eb10774800a042278df0
Askie Personal GitHub <isme408@gmail.com>
2021-06-24 16:33:40 +0800

    feat: create section using react hooks

--- react-hooks-develop/src/components/Tasks/CreateSection/CreateSection.component.jsx
+++ react-hooks-develop/src/components/Tasks/CreateSection/CreateSection.component.jsx
@@ -1,29 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './CreateSection.component.scss';
 
-const CreateSection = () => {
+const CreateSection = ({ handleCreateTodo }) => {
+  const [todo, setTodo] = useState('');
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    if (message && todo) {
+      setMessage('');
+    }
+  }, [message, todo]);
+
+  const handleSubmitNewTodo = (e, newTodo) => {
+    e.preventDefault();
+
+    if (!newTodo) {
+      setMessage('你沒輸入內容...!!!!');
+      return;
+    }
+
+    handleCreateTodo(e, newTodo);
+    setTodo('');
+  };
+
   return (
     <section className='create'>
       <h2>
         待辦事項
-        <span
-          className='msgBox' //ref='msgBox'
-        />
+        {message ? <span className='msgBox'>{message}</span> : null}
       </h2>
-      <input
-        type='text'
-        placeholder='例如：倒垃圾'
-        className='addInput'
-        autoFocus
-        // ref='addInput'
-        // onKeyup='createTodo'
-      />
-      <input
-        type='submit'
-        className='task__button task__submit'
-        value='新增'
-        // onClick='createTodo'
-      />
+      <form onSubmit={(e) => handleSubmitNewTodo(e, todo)}>
+        <input
+          type='text'
+          placeholder='例如：倒垃圾'
+          className='addInput'
+          autoFocus
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <input
+          type='submit'
+          className='task__button task__submit'
+          value='新增'
+        />
+      </form>
     </section>
   );
 };
--- react-hooks-develop/src/components/Tasks/Tasks.component.jsx
+++ react-hooks-develop/src/components/Tasks/Tasks.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Tasks.component.scss';
 
 import CreateSection from './CreateSection/CreateSection.component';
@@ -6,12 +6,25 @@ import TodoTasks from './Todo/TodoTasks.component';
 import CompletedTasks from './Completed/CompletedTasks.component';
 
 const Tasks = () => {
+  const [todos, setTodos] = useState([]);
+
+  const handleCreateTodo = (e, todo) => {
+    setTodos([
+      ...todos,
+      {
+        id: Date.now(),
+        todo,
+        isTodoCompleted: false,
+      },
+    ]);
+  };
+
   return (
     <article className='content'>
       <header>
         <h1>TO.DO</h1>
-        <CreateSection />
-        <TodoTasks />
+        <CreateSection handleCreateTodo={handleCreateTodo} />
+        <TodoTasks todos={todos} />
         <CompletedTasks />
       </header>
     </article>
